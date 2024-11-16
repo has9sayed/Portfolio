6220b2000dfd5914ff9bc61814f4b5a57ffd4130
has9sayed <wkhasnainup@gmail.com>
2024-11-16 12:24:22 +0600

    Fix static files configuration for deployment

--- build.sh
+++ build.sh
@@ -4,5 +4,11 @@ set -o errexit
 
 pip install -r requirements.txt
 
+# Create staticfiles_build directory
+mkdir -p staticfiles_build
+
+# Copy your existing static files to the new directory
+cp -r staticfiles/* staticfiles_build/
+
 python manage.py collectstatic --no-input
 python manage.py migrate 
--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-fx(uh_$c1o%9ft+7lm-&)$m)3r*6&96cuorg!_%ylg9ts3#5ol'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-fx(uh_$c1o%9ft+7lm-&)$m)3r*6&96cuorg!_%ylg9ts3#5ol')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,11 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-MEDIA_URL = '/images/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles')
-]
+# Simplified static file serving
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
